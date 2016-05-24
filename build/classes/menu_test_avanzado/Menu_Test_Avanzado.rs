f8a7554f61f1b144c82dcb35ab2af28cf547f3a5
german <germanjongewaard@gmail.com>
2016-05-24 22:04:55 +0200

    Simple menu

---
+++ build/classes/menu_test_avanzado/Menu_Test_Avanzado.rs
@@ -0,0 +1,6 @@
+menu_Test_Avanzado.Menu_Test_Avanzado
+menu_Test_Avanzado.MenuProcesador_III
+video114.MenuProcesador_III
+menu_Test_Avanzado.LaminaProcesador
+video114.LaminaProcesador
+video114.Menu_Test_Avanzado
